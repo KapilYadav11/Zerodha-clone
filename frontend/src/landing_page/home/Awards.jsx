b84e433dd8b2948f61113ff2b0fcc20b51b66609
Kapil <kapily1011@gmail.com>
2026-01-02 20:46:03 +0530

    award and stats done

--- frontend/src/landing_page/home/Awards.jsx
+++ frontend/src/landing_page/home/Awards.jsx
@@ -2,31 +2,33 @@ import React from 'react'
 
 const Awards = () => {
   return (
-    <div className='mt-34 '>
-      <div className='ml-12 flex flex-col justify-center'>
-        <div>
+    <div className='mt-40  '>
+      <div className='ml-12 flex justify-between items-center '>
+        <div className='ml-22'>
           <img src="media/images/largestBroker.svg" alt="" />
         </div>
-        <div>
-          <h1>Largest Stock broker in India</h1>
-        </div>
-        <div>
-          <p>2+ million Zerodha clients contribute to over 15% of all retail order volumens in India daily by trading and investing in:</p>
-        </div>
-        <div>
-          <div>
-            <ul>
-              <li>Future and Options</li>
-              <li>Commodity derivatives</li>
-              <li>Currency derivatives</li>
-            </ul>
+        <div className='m-10 flex justify-between items-center gap-6 flex-col'>
+          <div className='mr-30'>
+            <h1 className='font-semibold text-3xl mr-18'>Largest Stock broker in <span className='font-bold text-orange-300'> India</span></h1>
           </div>
           <div>
-            <ul>
-              <li>stocks & IPOs</li>
-              <li>Direct mutual funds</li>
-              <li>Bonds and Growth</li>
-            </ul>
+            <p className='text-lg font-semibold mb-14 mr-20'>2+ million Zerodha clients contribute to over 15% of  all retail <br></br> order volumens in India daily by trading and investing in:</p>
+          </div>
+          <div className='flex justify-center items-center mr-14 gap-18 text-lg'>
+            <div>
+              <ul className='list-disc pl-5 space-y-1 text-gray-700'>
+                <li>Future and Options</li>
+                <li>Commodity derivatives</li>
+                <li>Currency derivatives</li>
+              </ul>
+            </div>
+            <div  className="">
+              <ul className='list-disc pl-5 space-y-1 text-gray-700'>
+                <li>Stocks & IPOs</li>
+                <li>Direct mutual funds</li>
+                <li>Bonds and Growth</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/landing_page/home/Stats.jsx
+++ frontend/src/landing_page/home/Stats.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
 
 const Stats = () => {
   return (
-    <div>Stats</div>
-  )
-}
+    <div div className='mt-40  '>
+      <div className="flex justify-between items-center ">
+        <div className="flex justify-center items-center ml-35 mb-30 ">
+          <div className="flex gap-3 flex-col">
+            <h1 className="text-3xl font-bold mb-1">Trust with confidence</h1>
+            <h2 className="text-2xl font-semibold mt-1">Customer-first always</h2>
+            <p className="text-lg mb-8 text-gray-500">
+              That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh<br></br>
+              crores of equity investments, making us India’s largest broker;<br></br>
+              contributing to 15% of daily retail exchange volumes in India.
+            </p>
+            <h2 className="text-2xl font-semibold mt-2">No spam or gimmicks</h2>
+            <p className="text-lg mb-8 text-gray-500">
+              No gimmicks, spam, "gamification", or annoying push notifications.<br></br>
+              High quality apps that you use at your pace, the way you like. Our<br></br>
+              philosophies.
+            </p>
+            <h2 className="text-2xl font-semibold">The <span className="text-sky-700">Zerodha</span> universe</h2>
+            <p className="text-lg mb-8 text-gray-500">
+              Not just an app, but a whole ecosystem. Our investments in 30+<br></br>
+              fintech startups offer you tailored services specific to your<br></br>
+              needs.
+            </p>
+            <h2 className="text-2xl font-semibold">Do better with money</h2>
+            <p className="text-lg mb-6 text-gray-500">
+              With initiatives like Nudge and Kill Switch, we don't just<br></br>
+              facilitate transactions, but actively help you do better with your<br></br>
+              money.
+            </p>
+          </div>
+        </div>
+        <div className="mr-35 mb-25 w-[50%] h-[75%]">
+          <img src="media/images/ecosystem.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Stats
+export default Stats;
