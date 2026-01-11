2c3660737352c54b8b88f35a9318ed215f7f7860
Kapil <kapily1011@gmail.com>
2026-01-11 17:06:27 +0530

    done routing

--- frontend/src/landing_page/Footer.jsx
+++ frontend/src/landing_page/Footer.jsx
@@ -1,60 +1,211 @@
-import React from 'react'
+import React from "react";
 
 const footer = () => {
   return (
-    <div className='mt-40 py-8  gap-60 shadow-[0_10px_20px_rgba(0,0,0,0.1) bg-gray-50 rounded'>
-      <div className='flex justify-center   '>
+    <div className="mt-40 py-8  gap-60 shadow-[0_10px_20px_rgba(0,0,0,0.1) bg-gray-100 rounded">
+      <div className="flex justify-center   ">
         <div>
-          <img src="media/images/logo.svg" className='w-[35%] ml-35 py-3' alt="" />
-          <p className='text-sm px-35 text-gray-500 whitespace-nowrap'>&copy; 2010 - 2025, Zerodha Broking Ltd. <br />
-
-All rights reserved</p>
+          <img
+            src="media/images/logo.svg"
+            className="w-[35%] ml-35 py-3"
+            alt=""
+          />
+          <p className="text-sm px-35 text-gray-500 whitespace-nowrap">
+            &copy; 2010 - 2025, Zerodha Broking Ltd. <br />
+            All rights reserved
+          </p>
         </div>
-        <div className='flex flex-col gap-2 whitespace-nowrap mr-20'>
-          <h1 className='text-black text-2xl '>Account</h1>
-          <a href='' className='text-gray-500'> Open demat account</a>
-          <a href=''className='text-gray-500' > Minor demat account</a>
-          <a href='' className='text-gray-500'> NRI demat account</a>
-          <a href=''className='text-gray-500' > Commodity</a>
-          <a href=''className='text-gray-500' > Dematerialisation</a>
-          <a href=''className='text-gray-500' > Fund transfer</a>
-          <a href='' className='text-gray-500'> MTF</a>
-          <a href='' className='text-gray-500' > Referral program</a>
+        <div className="flex flex-col gap-2 whitespace-nowrap mr-20">
+          <h1 className="text-black text-2xl ">Account</h1>
+          <a href="" className="text-gray-500">
+            {" "}
+            Open demat account
+          </a>
+          <a href="" className="text-gray-500">
+            {" "}
+            Minor demat account
+          </a>
+          <a href="" className="text-gray-500">
+            {" "}
+            NRI demat account
+          </a>
+          <a href="" className="text-gray-500">
+            {" "}
+            Commodity
+          </a>
+          <a href="" className="text-gray-500">
+            {" "}
+            Dematerialisation
+          </a>
+          <a href="" className="text-gray-500">
+            {" "}
+            Fund transfer
+          </a>
+          <a href="" className="text-gray-500">
+            {" "}
+            MTF
+          </a>
+          <a href="" className="text-gray-500">
+            {" "}
+            Referral program
+          </a>
         </div>
-        <div className='flex flex-col gap-2 whitespace-nowrap mr-20'>
-          <h1  className='text-black text-2xl '>Support</h1>
-          <a href='' className='text-gray-500' >Contact us</a>
-          <a href='' className='text-gray-500' >Support portal</a>
-          <a href='' className='text-gray-500' >How to file a complaint?</a>
-          <a href='' className='text-gray-500' >Status of your complaints</a>
-          <a href='' className='text-gray-500'>Bulletin</a>
-          <a href='' className='text-gray-500'>Circular</a>
-          <a href='' className='text-gray-500'>Z-Connect blog</a>
-          <a href='' className='text-gray-500'>Downloads</a>
+        <div className="flex flex-col gap-2 whitespace-nowrap mr-20">
+          <h1 className="text-black text-2xl ">Support</h1>
+          <a href="" className="text-gray-500">
+            Contact us
+          </a>
+          <a href="" className="text-gray-500">
+            Support portal
+          </a>
+          <a href="" className="text-gray-500">
+            How to file a complaint?
+          </a>
+          <a href="" className="text-gray-500">
+            Status of your complaints
+          </a>
+          <a href="" className="text-gray-500">
+            Bulletin
+          </a>
+          <a href="" className="text-gray-500">
+            Circular
+          </a>
+          <a href="" className="text-gray-500">
+            Z-Connect blog
+          </a>
+          <a href="" className="text-gray-500">
+            Downloads
+          </a>
         </div>
-        <div className='flex flex-col gap-2 whitespace-nowrap mr-20'>
-          <h1 className='text-black text-2xl '>Company</h1>
-          <a href=''className='text-gray-500'>About</a>
-          <a href=''className='text-gray-500'>Philosophy</a>
-          <a href=''className='text-gray-500'>Press & media</a>
-          <a href=''className='text-gray-500'>Careers</a>
-          <a href=''className='text-gray-500'>Zerodha Cares (CSR)</a>
-          <a href=''className='text-gray-500'>Zerodha.tech</a>
-          <a href=''className='text-gray-500'>Open source</a>
+        <div className="flex flex-col gap-2 whitespace-nowrap mr-20">
+          <h1 className="text-black text-2xl ">Company</h1>
+          <a href="" className="text-gray-500">
+            About
+          </a>
+          <a href="" className="text-gray-500">
+            Philosophy
+          </a>
+          <a href="" className="text-gray-500">
+            Press & media
+          </a>
+          <a href="" className="text-gray-500">
+            Careers
+          </a>
+          <a href="" className="text-gray-500">
+            Zerodha Cares (CSR)
+          </a>
+          <a href="" className="text-gray-500">
+            Zerodha.tech
+          </a>
+          <a href="" className="text-gray-500">
+            Open source
+          </a>
         </div>
-        <div className='flex flex-col gap-2 whitespace-nowrap mr-60'>
-          <h1 className='text-black text-2xl '>Quick Links</h1>
-          <a href='' className='text-gray-500'>Upcoming IPOs</a>
-          <a href=''className='text-gray-500'>Brokerage charges</a>
-          <a href='' className='text-gray-500'>Market holidays</a>
-          <a href='' className='text-gray-500'>Economic calendar</a>
-          <a href='' className='text-gray-500'>Calculators</a>
-          <a href=''className='text-gray-500'>Markets</a>
-          <a href=''className='text-gray-500'>Sectors</a>
+        <div className="flex flex-col gap-2 whitespace-nowrap mr-60">
+          <h1 className="text-black text-2xl ">Quick Links</h1>
+          <a href="" className="text-gray-500">
+            Upcoming IPOs
+          </a>
+          <a href="" className="text-gray-500">
+            Brokerage charges
+          </a>
+          <a href="" className="text-gray-500">
+            Market holidays
+          </a>
+          <a href="" className="text-gray-500">
+            Economic calendar
+          </a>
+          <a href="" className="text-gray-500">
+            Calculators
+          </a>
+          <a href="" className="text-gray-500">
+            Markets
+          </a>
+          <a href="" className="text-gray-500">
+            Sectors
+          </a>
+        </div>
+      </div>
+      <div className="py-8 px-20 text-xs ml-6  tracking-wide text-gray-500">
+        <p>
+          Zerodha Broking Ltd.: Member of NSE, BSE​ &​ MCX – SEBI Registration
+          no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha
+          Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered
+          Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony,
+          Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078,
+          Karnataka, India. For any complaints pertaining to securities broking
+          please write to <span className="text-sky-600 cursor-pointer"> <a href="">complaints@zerodha.com</a></span> complaints@zerodha.com, for DP related to
+         <span className="text-sky-600 cursor-pointer"><a href="">dp@zerodha.com</a></span> . Please ensure you carefully read the Risk Disclosure
+          Document as prescribed by SEBI | ICF
+        </p>
+        <br />
+        <p>
+          Procedure to file a complaint on <span className="text-sky-600 cursor-pointer"><a href="">SEBI SCORES</a></span>: Register on SCORES
+          portal. Mandatory details for filing complaints on SCORES: Name, PAN,
+          Address, Mobile Number, E-mail ID. Benefits: Effective Communication,
+          Speedy redressal of the grievances
+        </p>
+        <br />
+        <p className="text-sky-600 cursor-pointer">Smart Online Dispute Resolution | Grievances Redressal Mechanism</p>
+        <p>
+          Investments in securities market are subject to market risks; read all
+          the related documents carefully before investing.
+        </p>
+        <br />
+        <p>
+          Attention investors: 1) Stock brokers can accept securities as margins
+          from clients only by way of pledge in the depository system w.e.f
+          September 01, 2020. 2) Update your e-mail and phone number with your
+          stock broker / depository participant and receive OTP directly from
+          depository on your e-mail and/or mobile number to create pledge. 3)
+          Check your securities / MF / bonds in the consolidated account
+          statement issued by NSDL/CDSL every month.
+        </p>
+        <br />
+        <p>
+          India's largest broker based on networth as per NSE. <span  className="text-sky-600 cursor-pointer"><a href="">. NSE broker factsheet</a></span>
+        </p>
+        <br />
+        <p>
+          "Prevent unauthorised transactions in your account. Update your mobile
+          numbers/email IDs with your stock brokers. Receive information of your
+          transactions directly from Exchange on your mobile/email at the end of
+          the day. Issued in the interest of investors. KYC is one time exercise
+          while dealing in securities markets - once KYC is done through a SEBI
+          registered intermediary (broker, DP, Mutual Fund etc.), you need not
+          undergo the same process again when you approach another
+          intermediary." Dear Investor, if you are subscribing to an IPO, there
+          is no need to issue a cheque. Please write the Bank account number and
+          sign the IPO application form to authorize your bank to make payment
+          in case of allotment. In case of non allotment the funds will remain
+          in your bank account. As a business we don't give stock tips, and have
+          not authorized anyone to trade on behalf of others. If you find anyone
+          claiming to be part of Zerodha and offering such services, please
+         <span className="text-sky-600 cursor-pointer"><a href=""> create a ticket here.</a></span>
+        </p>
+        <br />
+        <p>
+          *Customers availing insurance advisory services offered by Ditto
+          (Tacterial Consulting Private Limited | IRDAI Registered Corporate
+          Agent (Composite) License No CA0738) will not have access to the
+          exchange investor grievance redressal forum, SEBI SCORES/ODR, or
+          arbitration mechanism for such products.
+        </p>
+        <br />
+        <div className="flex gap-9  text-lg" >
+          <a  className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">NSE</a>
+          <a className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">BSE</a>
+          <a className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">MCX</a>
+          <a className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">Terms & conditions</a>
+          <a className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">Policies & procedures</a>
+          <a className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">Privacy policy</a>
+          <a href="">Disclosure</a>
+          <a className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">For investor's attention</a>
+          <a className="block text-gray-500 hover:text-sky-700 transition-colors duration-200 cursor-pointer" href="">Investor charter</a>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,25 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import HomePage from './landing_page/home/HomePage.jsx'
+import Signup from './landing_page/signup/Signup.jsx'
+import AboutPage from './landing_page/about/AboutPage.jsx'
+import PricingPage from './landing_page/pricing/PricingPage.jsx'
+import SupportPage from './landing_page/supports/SupportPage.jsx'
+import ProductPage from './landing_page/products/ProductPage.jsx'
 
 createRoot(document.getElementById('root')).render(
- 
-   
-    <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="signup" element={<Signup />} />
+      <Route path="about" element={<AboutPage />} />
+      <Route path="product" element={<ProductPage />} />
+      <Route path="pricing" element={<PricingPage />} />
+      <Route path="support" element={<SupportPage />} />
+      
 
-
+    </Routes>
+  </BrowserRouter>
 )
